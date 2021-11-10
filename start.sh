705c577b35724a56170ece928f71f8a2fb1cec98
Duckoi1707 <91252600+Duckoi1707@users.noreply.github.com>
2021-11-10 22:52:33 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ else
   echo "Cloning $BRANCH branch...."
   git clone https://github.com/Duckoi1707/Live/tree/patch-2 -b $BRANCH /Live
 fi
-cd /VideoPlayerTG
+cd /Live
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
