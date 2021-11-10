f4595a2928f322ce0d010588eaa044100daebbc7
Duckoi1707 <91252600+Duckoi1707@users.noreply.github.com>
2021-11-10 22:52:17 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/telegramcode/VideoPlayerTG /VideoPlayerTG
+  git clone https://github.com/Duckoi1707/Live/tree/patch-2 /Live
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/telegramcode/VideoPlayerTG -b $BRANCH /VideoPlayerTG
+  git clone https://github.com/Duckoi1707/Live/tree/patch-2 -b $BRANCH /Live
 fi
 cd /VideoPlayerTG
 pip3 install -U -r requirements.txt
